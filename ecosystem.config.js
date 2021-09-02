1688358ae3eebe31965e3a2450355387024f5f65
Danang Galuh Tegar Prasetyo <connect@danang.id>
2021-09-03 03:39:27 +0700

    Fix formatting
    Signed-off-by: Danang Galuh Tegar Prasetyo <connect@danang.id>

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  apps : [{
-    name: 'realtime-heart-rate',
-    script: 'lib/app.js',
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }],
+	apps : [{
+		name: 'realtime-heart-rate',
+		script: 'lib/app.js',
+		autorestart: true,
+		watch: false,
+		max_memory_restart: '1G',
+		env: {
+			NODE_ENV: 'development'
+		},
+		env_production: {
+			NODE_ENV: 'production'
+		}
+	}],
 };
